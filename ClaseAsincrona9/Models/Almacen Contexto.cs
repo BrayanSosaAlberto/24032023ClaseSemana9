aa11bb2947d38b92b869b387db61d2f186367921
BrayanSosaAlberto <125327983+BrayanSosaAlberto@users.noreply.github.com>
2023-03-24 17:14:43 -0600

    Asincrona9

---
+++ ClaseAsincrona9/Models/Almacen Contexto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseAsincrona9.Models
+{
+    internal class Almacen_Contexto
+    {
+    }
+}
